1973fee6c870d1bb8a2c1406926f45118fc7aeef
Matias Martinez <vittfiles@gmail.com>
2023-12-02 16:20:40 -0300

    some animations were added

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,25 @@ module.exports = {
   ],
   theme: {
     extend: {
+      keyframes: {
+        'show': {
+          '0%': { transform: 'scale(1,0)' },
+          '100%': { transform: 'scale(1,1)' }
+        },
+        'show-inv': {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' }
+        },
+        'show-d': {
+          '0%': { background: 'rgba(255,255,255,0)' },
+          '100%': { background: 'rgba(255,255,255,1)' }
+        }
+      },
+      animation: {
+        'show': 'show .3s ease-out normal',
+        'show-inv': 'show-inv .7s ease-out normal',
+        'show-d': 'show-d .7s ease-out normal',
+      },
       colors: {
         pastel: '#F2B3D6',
         purple: '#B7AEF2',
